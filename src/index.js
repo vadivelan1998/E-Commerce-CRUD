302d606cede67b6d665fbbf75d2572ca77955c62
vadivelan1998 <vadivelan1998@gmail.com>
2022-06-09 22:56:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const express=require("express")
 const app=express()
+const userController=require("./controllers/user.controller")
+
+
+app.use(express.json())
+
+
+app.use("/users",userController)
+
 
 
 module.exports=app
