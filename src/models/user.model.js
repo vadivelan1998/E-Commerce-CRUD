c260b1c1d717eac6cb2c860e6c4835add52479b5
vadivelan1998 <vadivelan1998@gmail.com>
2022-06-09 21:41:28 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,25 @@
 const mongoose=require("mongoose")
-const userSchema=new mongoose.Schema(
-    {
-        first_name:{type:String,required:true},
-        last_name={ty}
-    },
-    {}
-)
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    address: [
+      {
+        doorNo: { type: String, required: true },
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+        country: { type: String, required: true },
+        pinCode: { type: String, required: true },
+      },
+    ],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const User=mongoose.model(userSchema)
+module.exports=User
